85cd1d591e21ad589846d50603180e64a16866de
Nicusor97 <nicolaepicatureanu@gmail.com>
2019-01-08 09:52:12 +0200

    added compatibility for all pytest versions

--- pytest_httpretty.py
+++ pytest_httpretty.py
@@ -1,9 +1,21 @@
+from distutils.version import LooseVersion
 import functools
 
 import httpretty
-
+import pytest
 
 __version__ = '0.2.0'
+
+
+def get_marker_method(item):
+    _marker_method = None
+    pytest_version = LooseVersion("{}".format(pytest.__version__))
+    if pytest_version < LooseVersion("4.1.0"):
+        _marker_method = item.get_marker('httpretty')
+    else:
+        _marker_method = item.get_closest_marker('httpretty')
+
+    return _marker_method
 
 
 def pytest_configure(config):
@@ -12,14 +24,14 @@ def pytest_configure(config):
 
 
 def pytest_runtest_setup(item):
-    marker = item.get_closest_marker('httpretty')
+    marker = get_marker_method(item)
     if marker is not None:
         httpretty.reset()
         httpretty.enable()
 
 
 def pytest_runtest_teardown(item, nextitem):
-    marker = item.get_closest_marker('httpretty')
+    marker = get_marker_method(item)
     if marker is not None:
         httpretty.disable()
 
